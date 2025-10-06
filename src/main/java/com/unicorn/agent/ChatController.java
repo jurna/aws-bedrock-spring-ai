f16c61f56cfc495bfc79c75a70661b78c41126d0
workshop-user <workshop-user@example.com>
2025-10-06 13:00:50 +0000

    Add local memory

--- src/main/java/com/unicorn/agent/ChatController.java
+++ src/main/java/com/unicorn/agent/ChatController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 
 import reactor.core.publisher.Flux;
 
@@ -20,8 +22,12 @@ public class ChatController {
 	private final ChatClient chatClient;
 
 	public ChatController (ChatClient.Builder chatClient){
+        var chatMemory = MessageWindowChatMemory.builder()
+			.maxMessages(20)
+			.build();
 		this.chatClient = chatClient
 			.defaultSystem(DEFAULT_SYSTEM_PROMPT)
+            .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
 			.build();
 	}
 
